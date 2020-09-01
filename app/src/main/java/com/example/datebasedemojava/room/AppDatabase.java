02a32af7781acad03edb11f29e62104024fac127
Allen <yangle@julive.com>
2020-09-01 17:31:25 +0800

    数据库降/升级处理

--- app/src/main/java/com/example/datebasedemojava/room/AppDatabase.java
+++ app/src/main/java/com/example/datebasedemojava/room/AppDatabase.java
@@ -3,15 +3,17 @@ package com.example.datebasedemojava.room;
 
 import android.content.Context;
 
+import androidx.annotation.NonNull;
 import androidx.room.Database;
 import androidx.room.Room;
 import androidx.room.RoomDatabase;
-import androidx.room.TypeConverter;
 import androidx.room.TypeConverters;
+import androidx.room.migration.Migration;
+import androidx.sqlite.db.SupportSQLiteDatabase;
 
 import static com.example.datebasedemojava.room.Constant.DATABASE_NAME;
 
-@Database(entities = {User.class,CustomType.class}, version = 2, exportSchema = false)
+@Database(entities = {User.class, CustomType.class}, version = 2, exportSchema = false)
 @TypeConverters(DateConverter.class)
 public abstract class AppDatabase extends RoomDatabase {
     public abstract UserDao userDao();
@@ -26,9 +28,19 @@ public abstract class AppDatabase extends RoomDatabase {
     }
 
     private static AppDatabase create(final Context context) {
+        // addMigrations   设置数据库升级/降级操作
         return Room.databaseBuilder(
                 context,
                 AppDatabase.class,
-                DATABASE_NAME).build();
+                DATABASE_NAME)
+                .addMigrations(new Migration(1, 2) {
+            //1. startVersion表示的是升级开始的版本，endVersion表示要升级到的版本
+            //2. 同时需要将 @Database 注解的version的值改为 endVersion
+            @Override
+            public void migrate(@NonNull SupportSQLiteDatabase database) {
+                //这里对表或者数据的操作， eg:复制，迁移 删除 修改表等
+//                database.execSQL("ALTER TABLE TableC RENAME TO TableD_temp");
+            }
+        }).build();
     }
 }
